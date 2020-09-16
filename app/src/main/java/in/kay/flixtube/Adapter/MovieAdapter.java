ad5e7aff5a8dd24ff1b1cb4139ee2c79f4aa9687
Kapil <kapil1910086@akgec.ac.in>
2020-09-16 21:46:14 +0530

    Connected with firebase and added movie adapter.

---
+++ app/src/main/java/in/kay/flixtube/Adapter/MovieAdapter.java
@@ -0,0 +1,93 @@
+package in.kay.flixtube.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+
+import in.kay.flixtube.Model.MovieModel;
+import in.kay.flixtube.R;
+
+public class MovieAdapter extends FirebaseRecyclerAdapter<MovieModel , MovieAdapter.movieAdapterViewHolder> {
+    public MovieAdapter(@NonNull FirebaseRecyclerOptions<MovieModel> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    public Context context;
+    public MovieAdapter(@NonNull FirebaseRecyclerOptions<MovieModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull final movieAdapterViewHolder holder, final int position, @NonNull final MovieModel model) {
+        Typeface medium = Typeface.createFromAsset(context.getAssets(), "Gilroy-Medium.ttf");
+        Typeface regular = Typeface.createFromAsset(context.getAssets(), "Gilroy-Regular.ttf");
+        holder.title.setText(model.getTitle());
+        holder.title.setTypeface(medium);
+        holder.genre.setText(model.getCategory());
+        holder.genre.setTypeface(regular);
+        Picasso.get()
+                .load(model.getImage())
+                .networkPolicy(NetworkPolicy.OFFLINE)
+                .into(holder.img, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+
+                        Picasso.get()
+                                .load(model.getImage())
+                                .into(holder.img);
+                    }
+                });
+        if (model.getType().equalsIgnoreCase("free"))
+        {
+            holder.vip.setVisibility(View.GONE);
+        }
+        holder.v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public movieAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_view, parent , false);
+        return  new movieAdapterViewHolder(view);
+    }
+
+    public class  movieAdapterViewHolder extends RecyclerView.ViewHolder {
+        TextView title,genre;
+        View v;
+        ImageView img,vip;
+        public movieAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title=itemView.findViewById(R.id.tv_movie_name);
+            genre=itemView.findViewById(R.id.tv_genre);
+            img=itemView.findViewById(R.id.iv_cover_img);
+            vip=itemView.findViewById(R.id.iv_vip);
+            v = itemView;
+        }
+    }
+}
--- app/src/main/java/in/kay/flixtube/UI/MainActivity.java
+++ app/src/main/java/in/kay/flixtube/UI/MainActivity.java
@@ -1,16 +1,85 @@
 package in.kay.flixtube.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import in.kay.flixtube.Adapter.MovieAdapter;
+import in.kay.flixtube.Model.MovieModel;
 import in.kay.flixtube.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    DatabaseReference rootRef;
+    TextView tvName, tvFeatured, tvMovies ,tvSeries;
+    RecyclerView rvFeatured, rvMovies, rvSeries;
+    MovieAdapter movieAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initz();
+    }
+    private void initz() {
+        LoadViews();
+        LoadMovies();
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void LoadViews() {
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        /////
+        Typeface font = Typeface.createFromAsset(this.getAssets(), "Gilroy-ExtraBold.ttf");
+        Typeface typeface = Typeface.createFromAsset(this.getAssets(), "Montserrat_bold.ttf");
+        /////
+        tvName = findViewById(R.id.tv_name);
+        tvFeatured = findViewById(R.id.tv_featured);
+        tvSeries = findViewById(R.id.tv_series);
+        tvMovies = findViewById(R.id.tv_movies);
+        /////
+        rvFeatured = findViewById(R.id.rv_featured);
+        rvSeries = findViewById(R.id.rv_series);
+        rvMovies = findViewById(R.id.rv_movies);
+        /////
+        rvMovies.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        /////
+        tvName.setTypeface(font);
+        tvFeatured.setTypeface(typeface);
+        tvMovies.setTypeface(typeface);
+        tvSeries.setTypeface(typeface);
+    }
+    private void LoadMovies() {
+        FirebaseRecyclerOptions<MovieModel> options = new FirebaseRecyclerOptions.Builder<MovieModel>()
+                .setQuery(rootRef.child("Movies"), MovieModel.class)
+                .build();
+        movieAdapter = new MovieAdapter(options,this);
+        rvMovies.setAdapter(movieAdapter);
+        movieAdapter.startListening();
+    }
+    private void hideSystemUI() {
+        //https://developer.android.com/training/system-ui/immersive.html
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
