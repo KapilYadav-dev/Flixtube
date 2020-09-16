e6c2414ed56865fa2a9ba8dad44438c0cf274071
Kapil <kapil1910086@akgec.ac.in>
2020-09-16 22:04:56 +0530

    Added Detail activity.

--- app/src/main/java/in/kay/flixtube/UI/DetailActivity.java
+++ app/src/main/java/in/kay/flixtube/UI/DetailActivity.java
@@ -3,7 +3,6 @@ package in.kay.flixtube.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 import in.kay.flixtube.R;
 
@@ -13,11 +12,5 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
-
-    public void PlayMovie(View view) {
-    }
-
-    public void Download(View view) {
     }
 }
