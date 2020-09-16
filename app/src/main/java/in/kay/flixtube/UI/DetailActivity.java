2e04fb9e0cd1637bf7e3e9bf211c861cacf25e83
Kapil <kapil1910086@akgec.ac.in>
2020-09-16 22:03:29 +0530

    Added Offline handling capabilities to app

---
+++ app/src/main/java/in/kay/flixtube/UI/DetailActivity.java
@@ -0,0 +1,23 @@
+package in.kay.flixtube.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import in.kay.flixtube.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+    public void PlayMovie(View view) {
+    }
+
+    public void Download(View view) {
+    }
+}
